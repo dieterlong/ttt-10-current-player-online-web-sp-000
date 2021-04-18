0afa997aee05719e16126b084a9586d5c3bf34e6
dieter long <dieterwaitforitlong@gmail.com>
2021-04-18 17:54:37 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -8,12 +8,6 @@ def turn_count(board)
   turns
 end
 
-
-
    def current_player(board)
-     if turn_count(board).even? == "O"
-       "O"
-     else
-       "X"
-     end
-   end
+   turn_count(board).even? ? "X" : "O"
+      end
